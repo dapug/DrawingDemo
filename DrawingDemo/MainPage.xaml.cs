a4e99de2fc21dc3ed0f2e5647ef3aee287dbfea0
David Pugmire <roachslayer@hotmail.com>
2022-12-27 16:42:45 -0700

    Added file save ability using Skia

--- DrawingDemo/MainPage.xaml.cs
+++ DrawingDemo/MainPage.xaml.cs
@@ -1,6 +1,5 @@
-﻿using CommunityToolkit.Maui.Core;
-using System.Collections.ObjectModel;
-
+﻿using CommunityToolkit.Maui.Views;
+using Microsoft.Maui.Graphics.Skia;
 
 namespace DrawingDemo;
 
@@ -20,14 +19,31 @@ public partial class MainPage : ContentPage
 
     private async void drawView_DrawingLineCompleted(object sender, CommunityToolkit.Maui.Core.DrawingLineCompletedEventArgs e)
     {
-        var stream = await drawView.GetImageStream(200, 200);
-        // makes no difference what values I put in here. Same result with this:
-        // var stream = await drawView.GetImageStream(400, 20);
-
+        //var stream = await drawView.GetImageStream(200, 200);
+        var dvStream = await DrawingView.GetImageStream(drawView.Lines, new Size(200,200), Colors.LightBlue);
+        
         // ^ setting the stream size appears to do NOTHING ^
 
-        drawPreview.Source = ImageSource.FromStream(() => stream);
+        string fileName = Path.Combine(FileSystem.Current.AppDataDirectory, "mypic.png");
 
+        // This wont work, results in a blank file due to no format specified (and no ability to specify it. yay)
+        //using (var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+        //{
+        //    await dvStream.CopyToAsync(dvStream);
+        //}
+
+        // convert raw DrawingView stream to IImage so we can set the file format
+        var image = SkiaImage.FromStream(dvStream, ImageFormat.Png);
+            
+        using (var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+        {
+            await image.SaveAsync(fileStream);
+        }
+        
+        // dvStream.Position = 0;
+        // send the captured drawing out to UI, Image control
+        // (stopped working when I added file save, not sure why ??)
+        drawPreview.Source = ImageSource.FromStream(() => dvStream);
     }
 }
 
